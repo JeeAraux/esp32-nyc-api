73602a99ed279bfbfdd7f1c80941ac6a783403c6
JeeAraux <89745137+JeeAraux@users.noreply.github.com>
2025-02-26 03:34:48 +0100

    Update app.py

--- app.py
+++ app.py
@@ -52,8 +52,7 @@ class CustomJSONEncoder(json.JSONEncoder):
         return JSONEncoder.default(self, obj)
 
 mta = Mtapi(
-    app.config['MTA_KEY'],
-    app.config['STATIONS_FILE'],
+    stations_file=app.config['STATIONS_FILE'],
     max_trains=app.config['MAX_TRAINS'],
     max_minutes=app.config['MAX_MINUTES'],
     expires_seconds=app.config['CACHE_SECONDS'],
